79ed01f69f6166ba9399525c82ba9647b28273b6
Ben Bal <bensimonbal@gmail.com>
2021-12-04 15:14:13 +0100

    Chapter03

---
+++ Chapter03/BooleanOperators/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Console;
+
+namespace BooleanOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = true;
+            bool b = false;
+            WriteLine($"AND | a     | b ");
+            WriteLine($"a   | {a & a,-5} | {a & b,-5}");
+            WriteLine($"b   | {b & a,-5} | {b & b,-5}");
+            WriteLine($" ");
+            WriteLine($"OR  | a     | b ");
+            WriteLine($"a   | {a | a,-5} | {a | b,-5}");
+            WriteLine($"b   | {b | a,-5} | {b | b,-5}");
+            WriteLine($" ");
+            WriteLine($"XOR | a     | B ");
+            WriteLine($"a   | {a ^ a,-5} | {a ^ b,-5}");
+            WriteLine($"b   | {b ^ a,-5} | {b ^ b,-5}");
+            WriteLine($" ");
+            WriteLine($"a && Dostuff*() = {a && DoStuff()}");
+            WriteLine($"b && Dostuff*() = {b && DoStuff()}");
+
+        }
+
+            
+         private static bool DoStuff()
+         {
+            WriteLine("I am doing some stuff.");
+            return true;
+         }
+
+            
+    }
+}
---
+++ Chapter03/IterationStatements/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using static System.Console;
+
+namespace IterationStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+
+            while (x < 10)
+            {
+                WriteLine(x);
+                x++;
+            }
+            WriteLine();
+
+            for (int y = 1; y <= 10; y++)
+        {
+            WriteLine(y);
+        }
+        WriteLine();
+
+        string[] names = { "Adam", "Barry", "Charlie" };
+
+        foreach (string name in names)
+        {
+            WriteLine($"{name} has {name.Length} characters.");
+        }
+        WriteLine();
+
+            string password = string.Empty;
+
+            do
+            {
+                Write("Enter your password: ");
+                password = ReadLine();
+            }
+            while (password != "Pa$$w0rd");
+
+            WriteLine("Correct");
+        }
+    }
+}
